698b39fe5242f689134265a2cb7439f4df435516
Diego Calanzone <xdiego.wp@hotmail.com>
2021-08-16 16:41:56 +0200

    refactor: pytorch lightning support

---
+++ models_lightning/ae/main.py
@@ -0,0 +1,120 @@
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+import torch.nn.functional as F
+import torch.optim as optim
+
+from models.ae.ConvLSTMCell import ConvLSTMCell
+
+class deepSWE(pl.LightningModule):
+    def __init__(self, nf, in_chan, out_chan=1):
+        super(deepSWE, self).__init__()
+
+        self.encoder_1_convlstm = ConvLSTMCell(input_dim=in_chan,
+                                               hidden_dim=nf,
+                                               kernel_size=(3, 3),
+                                               bias=True)
+
+        self.encoder_2_convlstm = ConvLSTMCell(input_dim=nf,
+                                               hidden_dim=nf,
+                                               kernel_size=(3, 3),
+                                               bias=True)
+
+        self.decoder_1_convlstm = ConvLSTMCell(input_dim=nf,  # nf + 1
+                                               hidden_dim=nf,
+                                               kernel_size=(3, 3),
+                                               bias=True)
+
+        self.decoder_2_convlstm = ConvLSTMCell(input_dim=nf,
+                                               hidden_dim=nf,
+                                               kernel_size=(3, 3),
+                                               bias=True)
+
+        self.decoder_CNN = nn.Conv3d(in_channels=nf,
+                                     out_channels=out_chan,
+                                     kernel_size=(1, 3, 3),
+                                     padding=(0, 1, 1))
+
+    # ------------------------------------------------                                     
+
+    def mse_loss(self, input, target):
+        return F.mse_loss(input, target, reduction='sum') # reduction='sum'
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=1e-4)
+        return optimizer
+
+    def training_step(self, train_batch, batch_idx):
+        x, y = train_batch
+        
+        logits = self.forward(x, 1)
+
+        loss = self.mse_loss(logits[:, 0, 0, :, :] , y[:, 0, 0, :, :])
+        self.log('train_loss', loss)
+        return loss
+
+    def validation_step(self, val_batch, batch_idx):
+        x, y = val_batch
+
+        logits = self.forward(x, 1)
+
+        loss = self.mse_loss(logits[:, 0, 0, :, :] , y[:, 0, 0, :, :])
+        self.log('val_loss', loss)
+
+
+    # ------------------------------------------------
+
+    def autoencoder(self, x, seq_len, future_step, h_t, c_t, h_t2, c_t2, h_t3, c_t3, h_t4, c_t4):
+
+        outputs = []
+
+        # encoder
+        for t in range(seq_len):
+            h_t, c_t = self.encoder_1_convlstm(input_tensor=x[:, t, :, :],
+                                               cur_state=[h_t, c_t])  # we could concat to provide skip conn here
+            h_t2, c_t2 = self.encoder_2_convlstm(input_tensor=h_t,
+                                                 cur_state=[h_t2, c_t2])  # we could concat to provide skip conn here
+
+        # encoder_vector
+        encoder_vector = h_t2
+
+        # decoder
+        for t in range(future_step):
+            h_t3, c_t3 = self.decoder_1_convlstm(input_tensor=encoder_vector,
+                                                 cur_state=[h_t3, c_t3])  # we could concat to provide skip conn here
+            h_t4, c_t4 = self.decoder_2_convlstm(input_tensor=h_t3,
+                                                 cur_state=[h_t4, c_t4])  # we could concat to provide skip conn here
+            encoder_vector = h_t4
+            outputs += [h_t4]  # predictions
+
+        outputs = torch.stack(outputs, 1)
+        outputs = outputs.permute(0, 2, 1, 3, 4)
+        outputs = self.decoder_CNN(outputs)
+        outputs = torch.nn.LeakyReLU()(outputs)
+
+        return outputs
+
+    # ------------------------------------------------
+
+    def forward(self, x, future_seq=0, hidden_state=None):
+
+        """
+        Parameters
+        ----------
+        input_tensor:
+            5-D Tensor of shape (b, t, c, h, w)        #   batch, time, channel, height, width
+        """
+
+        # find size of different input dimensions
+        b, seq_len, _, h, w = x.size()
+
+        # initialize hidden states
+        h_t, c_t = self.encoder_1_convlstm.init_hidden(batch_size=b, image_size=(h, w))
+        h_t2, c_t2 = self.encoder_2_convlstm.init_hidden(batch_size=b, image_size=(h, w))
+        h_t3, c_t3 = self.decoder_1_convlstm.init_hidden(batch_size=b, image_size=(h, w))
+        h_t4, c_t4 = self.decoder_2_convlstm.init_hidden(batch_size=b, image_size=(h, w))
+
+        # autoencoder forward
+        outputs = self.autoencoder(x, seq_len, future_seq, h_t, c_t, h_t2, c_t2, h_t3, c_t3, h_t4, c_t4)
+
+        return outputs
---
+++ utils/dataloader_pl.py
@@ -0,0 +1,431 @@
+import numpy as np
+import os
+from tqdm import tqdm
+import math
+import cv2 as cv
+from utils.preprocessing import Preprocessing
+from sklearn.model_selection import train_test_split
+
+import torch as th
+from torch.utils.data import Dataset, random_split, DataLoader
+import pytorch_lightning as pl
+
+# ------------------------------------------------------------------------------
+
+class SWEDataModule(pl.LightningDataModule):
+
+    def __init__(self, root, past_frames, future_frames, test_size=0.1, val_size=0.1, partial=None):
+        super(SWEDataModule, self).__init__()
+
+        self.test_size = test_size
+        self.val_size = val_size
+        self.partial = partial
+        self.root = root
+        self.past_frames = past_frames
+        self.future_frames = future_frames
+        
+    def prepare_data(self):
+
+        dataset = SWEDataset(
+            root=self.root,
+            past_frames=self.past_frames, 
+            future_frames=self.future_frames,
+            partial=self.partial
+        )
+
+        test_len = int(max(1, len(dataset) * self.test_size))
+        val_len = int(max(1, len(dataset) * self.val_size))
+        train_len = len(dataset) - test_len - val_len
+        datasets = random_split(dataset, [train_len, test_len, val_len])
+
+        self.train_loader = DataLoader(datasets[0], batch_size=None, num_workers=0)
+        self.test_loader = DataLoader(datasets[1], batch_size=None, num_workers=0)
+        self.val_loader = DataLoader(datasets[2], batch_size=None, num_workers=0)
+
+    def train_dataloader(self):
+        return self.train_loader
+
+    def val_dataloader(self):
+        return self.val_loader
+
+    def test_dataloader(self):
+        return self.test_loader
+
+# ------------------------------------------------------------------------------
+
+class SWEDataset(Dataset):
+    def __init__(self, past_frames, future_frames, root, numpy_file=None, image_size=256, batch_size=4, dynamicity=1e-3, buffer_memory=100, buffer_size=1000, downsampling=False, partial=None, clipping_threshold=1e5, device="cpu"):
+        ''' Initiates the dataloading process '''
+
+        if root is None and numpy_file is None:
+            raise Exception("Please specify either a root path or a numpy file: -r /path -npy /dataset.py")
+
+        # Loads from disk
+        if numpy_file is None:
+            print("[~] Loading from disk...")
+
+            self.partitions = DataPartitions(
+                past_frames=past_frames,
+                future_frames=future_frames,
+                root=root,
+                partial=partial
+            )
+
+            self.dataset = DataGenerator(
+                root=root,
+                dataset_partitions=self.partitions.get_partitions(),
+                past_frames=self.partitions.past_frames,
+                future_frames=self.partitions.future_frames,
+                input_dim=(self.partitions.past_frames, image_size, image_size, 4),
+                output_dim=(self.partitions.future_frames, image_size, image_size, 3),
+                batch_size=batch_size,
+                buffer_size=buffer_size,
+                buffer_memory=buffer_memory,
+                downsampling=False,
+                dynamicity=dynamicity
+            )
+            self.X, self.Y, self.extra_batch = self.dataset.get_data()
+
+            self.X[self.X > clipping_threshold] = 0
+            self.Y[self.Y > clipping_threshold] = 0
+
+        # Loads from stored file
+        else:
+            print("[~] Loading from npy file...")
+
+            self.X, self.Y, self.extra_batch = np.load(numpy_file, allow_pickle=True)
+            print("[!] Successfully loaded dataset from {} \nX.shape: {}\nY.shape: {}\n".format(
+                numpy_file, self.X.shape, self.Y.shape
+            ))
+    
+        self.X = th.Tensor(self.X).to(device)
+        self.Y = th.Tensor(self.Y).to(device)
+
+        # Channel-first conversion
+        # b, s, t, h, w, c -> b, s, t, c, h, w
+        self.X = self.X.permute(0, 1, 2, 5, 3, 4)
+        self.Y = self.Y.permute(0, 1, 2, 5, 3, 4)
+
+        print("[!] Preprocessing completed!")
+
+    # ------------------------------
+
+    def __len__(self):
+        return self.X.shape[0]
+
+    def __getitem__(self, idx):
+        return (self.X[idx], self.Y[idx])
+           
+
+# ------------------------------------------------------------------------------
+
+class DataPartitions():
+    def __init__(self, past_frames, future_frames, root, partial=None):
+
+        self.root = root
+        self.past_frames = past_frames
+        self.future_frames = future_frames
+
+        self.dataset_partitions = []
+        self.areas = []
+        self.partial = partial
+
+        self.create_partitions()
+
+    def get_areas(self):
+        ''' Returns list of areas (source folder name)
+        '''
+        return self.areas
+
+    def get_partitions(self):
+        ''' Returns test dataset partitions
+            Partitions: [(ids_x(x, 10), ids_y(x, 4))]
+        '''
+
+        return self.dataset_partitions
+
+    def create_partitions(self):
+        ''' Creates the keras.DataLoader partitions (input-label)
+            - file format: mini-xxxx.DEP/VVX/VVY/BTM
+        '''
+
+        self.areas = os.listdir(self.root)
+        self.areas = [x for x in sorted(self.areas) if x.startswith("mini-") and os.path.isdir(self.root + x)]
+
+        if self.partial is not None:
+            self.areas = self.areas[:int(len(self.areas) * self.partial)]
+
+        if len(self.areas) <= 0:
+            raise Exception("Nessuna cartella area valida trovata.")
+
+        # Applica intervalli di interesse
+        for area in self.areas:
+
+            n_frames = len([x for x in os.listdir(self.root + area) if x.endswith(".DEP")])
+
+            # Fino ai frame che interessano
+            size = n_frames - self.past_frames - self.future_frames
+
+            partition_raw = []
+            labels = dict()
+
+            # -----------
+            for i in range(size):
+                partition_raw.append("id-{}".format(i))
+                labels["id-{}".format(i)] = list(
+                    range(i + (self.past_frames), i + (self.past_frames) + (self.future_frames)))
+
+            # folder_name, x_frame_id, y_frames_id
+            self.dataset_partitions.append((area, partition_raw, labels))
+
+
+# ------------------------------------------------------------------------------
+class DataGenerator():
+    def __init__(self, root, dataset_partitions, past_frames, future_frames, input_dim, output_dim,
+                 buffer_memory=1, buffer_size=100, batch_size=16, caching=True, downsampling=False, dynamicity=0.1):
+        '''
+            Data Generator
+            Inputs:
+
+                - Path containing folders of frames
+                - List of the names of these folders
+                - Partitions: [(ids_x(x, 10), ids_y(x, 4))]
+        '''
+
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+
+        self.dataset_partitions = dataset_partitions
+        self.batch_size = np.min([len(x[1]) for x in self.dataset_partitions]) # minimo numero di sequenze per area
+
+        self.past_frames = past_frames
+        self.future_frames = future_frames
+        self.caching = caching
+
+        self.batch_size = batch_size
+        self.blurry_filter_size = (3, 3)
+        self.downsampling = downsampling
+
+        self.root = root
+
+        self.buffer = []
+        self.buffer_size = buffer_size
+        self.buffer_memory = buffer_memory
+        self.buffer_hit_ratio = 0
+
+        self.preprocessing = Preprocessing()
+        self.dynamicity = dynamicity
+
+    def get_data(self):
+        'Generates batches of datapoints'
+        X, Y = self.__data_generation() # seq, t, h, w, c
+        ex_X = None
+        ex_Y = None
+
+        batch_residual = X.shape[0] % self.batch_size
+        n_batches = X.shape[0] // self.batch_size
+
+        # In case of n_sequences =/= k*batch_size
+        if batch_residual > 0:
+            X_b = X[:-batch_residual].reshape((n_batches, self.batch_size, *self.input_dim))
+            Y_b = Y[:-batch_residual].reshape((n_batches, self.batch_size, *self.output_dim))
+
+            # extra batch with n < batch_size
+            ex_X = np.array([[X[-batch_residual:]]])
+            ex_Y = np.array([[Y[-batch_residual:]]])
+
+        else:
+            X_b = X.reshape((n_batches, self.batch_size, *self.input_dim))
+            Y_b = Y.reshape((n_batches, self.batch_size, *self.output_dim))
+
+        return X_b, Y_b, (ex_X, ex_Y)
+
+
+    def __data_generation(self):
+        'Generates the raw sequence of datapoints (filtered)'
+
+        # stats
+        accesses = 0
+        hits = 0
+
+        # Initialization
+        X = None
+        Y = None
+
+        print("[x] {} areas found".format(len(self.dataset_partitions)))
+
+        # For each area
+        for area_index, area in enumerate(self.dataset_partitions):
+            # For each sequence
+            loaded = 0
+            for i, sequence in enumerate(area[1]):
+
+                # --- BTM
+                btm_filenames = [x for x in os.listdir(self.root + self.dataset_partitions[area_index][0]) if x.endswith(".BTM")]
+                if len(btm_filenames) == 0:
+                    raise Exception("No BTM map found for the area {}".format(self.dataset_partitions[area_index][0]))
+                btm = np.loadtxt(self.root + self.dataset_partitions[area_index][0] + "/" + btm_filenames[0])
+
+                # --- Preprocessing
+                if self.downsampling:
+                    btm = cv.GaussianBlur(btm, self.blurry_filter_size, 0)
+                    btm = cv.pyrDown(btm)
+
+                # riduzione valori il sottraendo minimo
+                min_btm = np.min(btm)
+                btm = btm - min_btm
+
+                btm.resize(btm.shape[0], btm.shape[1], 1)
+                btm_x = np.tile(btm, (self.past_frames, 1, 1, 1))
+
+                deps = None
+                vvx_s = None
+                vvy_s = None
+
+                framestart = int(sequence.replace("id-", ""))
+
+                # Starts from the right frame
+                for k in range(framestart, framestart + self.past_frames + self.future_frames):
+
+                    # id area -> id frame
+                    gid = "{}-{}-{}".format(area_index, sequence, k)
+
+                    # Parameters
+                    extensions = ["DEP", "VVX", "VVY"]
+                    matrices = []
+
+                    # Gets datapoint filename
+                    dep_filenames = [x for x in os.listdir(self.root + self.dataset_partitions[area_index][0]) if
+                                    x.endswith(".DEP")]
+
+                    if len(dep_filenames) == 0:
+                        raise Exception("No DEP maps found for the area {}".format(self.dataset_partitions[area_index][0]))
+
+                    # asserting that all maps are named with the same prefix
+                    dep_filename = dep_filenames[0].split(".")[0][:-4]
+
+                    # 1 frame -> 3 matrices (3 extensions)
+                    for i, ext in enumerate(extensions):
+                        accesses += 1
+                        global_id = "{}-{}".format(i, gid)  # indice linearizzato globale
+
+                        cache = self.buffer_lookup(
+                            global_id
+                        )
+                        if cache is False:
+                            frame = np.loadtxt(self.root + self.dataset_partitions[area_index][0] + "/{}{:04d}.{}".format(dep_filename, k, ext))
+                            # --- On-spot Gaussian Blurring
+                            if self.downsampling:
+                                frame = cv.GaussianBlur(frame, self.blurry_filter_size, 0)
+                                frame = cv.pyrDown(frame)
+                            # ----
+                            self.buffer_push(global_id, frame)
+                        else:
+                            frame = cache
+                            hits += 1
+                        matrices.append(frame)
+
+                    frame, vvx, vvy = matrices
+
+                    # ---
+                    velocity = np.sqrt(vvx ** 2 + vvy ** 2)
+
+                    if deps is None:
+                        deps = np.array([frame])
+                    else:
+                        deps = np.concatenate((deps, np.array([frame])))
+
+                    if vvx_s is None:
+                        vvx_s = np.array([vvx])
+                    else:
+                        vvx_s = np.concatenate((vvx_s, np.array([vvx])))
+
+                    if vvy_s is None:
+                        vvy_s = np.array([vvy])
+                    else:
+                        vvy_s = np.concatenate((vvy_s, np.array([vvy])))
+
+                # ---------
+
+                deps[deps > 10e5] = 0
+                vvx_s[vvx_s > 10e5] = 0
+                vvy_s[vvy_s > 10e5] = 0
+                btm_x[btm_x > 10e5] = 0
+
+                # --- X
+                x_dep = deps[:self.past_frames]
+                x_dep.resize((x_dep.shape[0], x_dep.shape[1], x_dep.shape[2], 1))
+
+                x_vx = vvx_s[:self.past_frames]
+                x_vx.resize((x_vx.shape[0], x_vx.shape[1], x_vx.shape[2], 1))
+
+                x_vy = vvy_s[:self.past_frames]
+                x_vy.resize((x_vy.shape[0], x_vy.shape[1], x_vy.shape[2], 1))
+
+                x = np.concatenate((x_dep, x_vx, x_vy, btm_x), axis=3)
+
+                # --- Y
+                y_dep = deps[self.past_frames:]
+                y_dep.resize((y_dep.shape[0], y_dep.shape[1], y_dep.shape[2], 1))
+
+                y_vx = vvx_s[self.past_frames:]
+                y_vx.resize((y_vx.shape[0], y_vx.shape[1], y_vx.shape[2], 1))
+
+                y_vy = vvy_s[self.past_frames:]
+                y_vy.resize((y_vy.shape[0], y_vy.shape[1], y_vy.shape[2], 1))
+
+                y = np.concatenate((y_dep, y_vx, y_vy), axis=3)
+
+                # filtering
+                sequence = np.concatenate((x[:,:,:,:3], y), axis=0)
+                score, valid = self.preprocessing.eval_datapoint(sequence, self.dynamicity)
+
+                if valid:
+
+                    loaded += 1
+
+                    if X is None: X = np.expand_dims(x,0)
+                    else: X = np.concatenate((X, np.expand_dims(x,0)))
+
+                    if Y is None: Y = np.expand_dims(y,0)
+                    else: Y = np.concatenate((Y, np.expand_dims(y,0)))
+
+                    print(". ", end = '')
+
+            print("\n[{}%] {} valid sequences loaded".format(round((area_index+1)/len(self.dataset_partitions)*100), loaded))
+
+        # Buffer ratio calculation
+        if accesses != 0:
+            self.buffer_hit_ratio = self.buffer_hit_ratio * 0.5 + 0.5 * (hits / accesses)
+
+        return X, Y
+
+    # ------------------------------------
+
+    def buffer_lookup(self, k):
+        ''' Get sequence (datapoint) from cache given the start frame global id '''
+
+        if self.caching:
+            for i, x in enumerate(self.buffer):
+                # Returns found record
+                if x["global_id"] == k:
+                    self.buffer[i]["fresh"] += 1
+                    return x["value"]
+
+                # Set any read record to 0 (second chance)
+                elif self.buffer[i]["fresh"] != 0:
+                    self.buffer[i]["fresh"] -= 1
+
+        return False
+
+    def buffer_push(self, k, x):
+        ''' Add sequence (datapoint) to cache with start frame global id '''
+
+        if self.caching:
+            # Makes space
+            if len(self.buffer) >= self.buffer_size:
+                for i, j in enumerate(self.buffer):
+                    if j["fresh"] == 0:
+                        del self.buffer[i]
+            # Push
+            self.buffer.append({'fresh': self.buffer_memory, 'global_id': k, 'value': x})
