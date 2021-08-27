bed78ec92210830f971af73bebbc3ec1684fbdc8
Diego Calanzone <diegocalanzone@gmail.com>
2021-08-27 17:43:55 +0200

    fixed testing

--- test_grid.py
+++ test_grid.py
@@ -10,6 +10,7 @@ import matplotlib.patches as patches
 
 import torch as th
 from torch.autograd import Variable
+import torch.nn as nn
 
 from models.experiments.nfnets import seq2seq_NFLSTM
 from models.ae import seq2seq_ConvLSTM
@@ -32,24 +33,6 @@ def str2bool(v):
         return False
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
-
-def reverse_ssim(y, y_true):
-    tot_ssim = 0
-    # for each sequence
-    for i, y_pred in enumerate(y):
-        # for each frame
-        for j, frame in enumerate(y_pred[0]):
-            y_h_pred = Variable(th.unsqueeze(th.unsqueeze(frame, 0), 0))
-            y_h_true = Variable(th.unsqueeze(th.unsqueeze(y_true[i, 0, j], 0), 0))
-
-            if th.cuda.is_available():
-                y_h_pred = y_h_pred.cuda()
-                y_h_true = y_h_true.cuda()
-
-            tot_ssim += pytorch_ssim.ssim(y_h_pred, y_h_true)
-
-    return 1 / (tot_ssim / len(y))
-
 # -------------------------------
 
 parser = argparse.ArgumentParser(description='Tests a train model against a given dataset')
@@ -61,11 +44,9 @@ parser.add_argument('-test_size', dest='test_size', default = None,
                     help='Test size for the split')
 parser.add_argument('-shuffle', dest='shuffle', default=True, type=str2bool,
                     help='Shuffle the dataset')
-parser.add_argument('-tf', dest='test_flight',
-                    help='Test flight. Avoids creating a train folder for this session.')
-parser.add_argument('-npy', dest='numpy_file',
-                    help='path to a npy stored dataset')
-parser.add_argument('-hidden_layers', dest='hidden_layers', default=4, type=int,
+parser.add_argument('-multigpu', dest='multigpu', default=False, type=str2bool,
+                    help='Supports multi-gpu models')
+parser.add_argument('-filters', dest='filters', default=4, type=int,
                     help='number of hidden layers')
 parser.add_argument('-in_channels', dest='in_channels', default=4, type=int,
                     help='number of input channels')
@@ -73,75 +54,65 @@ parser.add_argument('-out_channels', dest='out_channels', default=3, type=int,
                     help='number of input channels')                    
 parser.add_argument('-tests', dest='n_tests', default=10, type=int,
                     help='number of tests to perform')
-parser.add_argument('-weights', dest='weights_path',
+parser.add_argument('-weights', dest='weights_path', required=True,
                     help='model weights for testing')
 parser.add_argument('-dset', dest='dataset_path',
                     help='path to a npy stored dataset')
-parser.add_argument('-root', dest='root',
+parser.add_argument('-root', dest='root', required=True,
                     help='root path with the simulation files (cropped and stored in folders)')
+parser.add_argument('-partial', dest='partial', default=None, type=float,
+                    help='percentage of portion of dataset (to load partial, lighter chunks)')
+parser.add_argument('-image_size', dest='image_size', default=256, type=int,
+                    help='image size (width = height)')
+parser.add_argument('-batch_size', dest='batch_size', default=4, type=int,
+                    help='batch size')
+
 parser.add_argument('-p', dest='past_frames', default=4, type=int,
                     help='number of past frames')       
 parser.add_argument('-f', dest='future_frames', default=1, type=int, 
-                    help='number of future frames')       
-parser.add_argument('-partial', dest='partial', default=None, type=float,
-                    help='percentage of portion of dataset (to load partial, lighter chunks)')                                                            
-parser.add_argument('-i', dest='image_size', default=256, type=int,
-                    help='image size (width = height)')
-parser.add_argument('-b', dest='batch_size', default=4, type=int,
-                    help='batch size') 
+                    help='number of future frames')
 parser.add_argument('-d', dest='dynamicity', default=1e-3, type=float,
                     help='dynamicity rate (to filter out "dynamic" sequences)')                                                                                                  
 parser.add_argument('-bs', dest='buffer_size', default=1e3, type=float,
                     help='size of the cache memory (in entries)')
 parser.add_argument('-t', dest='buffer_memory', default=100, type=int,
                     help='temporal length of the cache memory (in iterations)')                                                                                                  
-parser.add_argument('-ds', dest='downsampling', default=False, type=str2bool, nargs='?',
-                    const=True, help='enable 2x downsampling (with gaussian filter)')  
-parser.add_argument('-ls', dest='latent_size', default=1024, type=int,
-                    help='latent size for the VAE')                                                                                                                                                      
 
 args = parser.parse_args()
-
-if args.root is None and args.numpy_file is None:
-    print("required: please specify a root path: -r /path")
-    exit()
-
-if args.weights_path is None:
-    print("required: please specify a weights path: -weights /*.weights")
-    exit()
 
 print("[~] Benchmark initialized, loading dataset...")
 
 # -------------- Setting up the run
 
-if args.test_flight is None:
-    num_run = len(os.listdir("runs/")) + 1
-    now = datetime.now()
-    foldername = "eval_{}_{}".format(num_run, now.strftime("%d_%m_%Y_%H_%M_%S"))
-    os.mkdir("runs/" + foldername)
+num_run = len(os.listdir("runs/")) + 1
+now = datetime.now()
+foldername = "eval_{}_{}".format(num_run, now.strftime("%d_%m_%Y_%H_%M_%S"))
+os.mkdir("runs/" + foldername)
 
 # -------------- Data definition
 if th.cuda.is_available():
     dev = "cuda:0"
 else:
     dev = "cpu"
+
 device = th.device(dev)
 
 plotsize = 15
 
 # -------------- Model
-if args.network == "conv":
-    net = seq2seq_ConvLSTM.EncoderDecoderConvLSTM(nf=args.hidden_layers, in_chan=args.in_channels, out_chan=args.out_channels).to(device) # False: many to one
-elif args.network == "nfnet":
-    net = seq2seq_NFLSTM.EncoderDecoderConvLSTM(nf=args.hidden_layers, in_chan=args.in_channels, out_chan=args.out_channels).to(device)
-else:
-    raise Exception("Unkown network type given.")
-
 # Loading model weights from previous training
 print("[x] Loading model weights")
+
+net = seq2seq_ConvLSTM.EncoderDecoderConvLSTM(nf=args.filters, in_chan=args.in_channels, out_chan=args.out_channels)
+
+if args.multigpu:
+    net = nn.DataParallel(net)
+
 net.load_state_dict(
-    th.load(args.weights_path, map_location=th.device('cpu'))
+    th.load(args.weights_path)
 )
+
+net.to(device)
 net.eval() # evaluation mode
 
 print("[!] Successfully loaded weights from {}".format(args.weights_path))
@@ -167,7 +138,7 @@ dataset = SWEDataset(
 
 for t in range(args.n_tests):
 
-    print("-- Test {} running...".format(t), end="")
+    print("-- Test {} running...".format(t), end="", flush=True)
 
     i = np.random.randint(len(dataset))       # random batch
     datapoint = dataset[i]
@@ -176,10 +147,13 @@ for t in range(args.n_tests):
         i = np.random.randint(len(dataset))       # random batch
         datapoint = dataset[i]
 
-    print("\t data loaded!")
+    print("\t data loaded!", flush=True)
     
     # b, t, c, h, w 
     x, y = datapoint
+
+    x = x.to(device)
+    y = y.to(device)
 
     start = time.time()
     # 1, t, c, h, w 
@@ -228,31 +202,34 @@ for t in range(args.n_tests):
     axs[i + 2].add_patch(rect)
     axs[i + 2].title.set_text('Ground Truth')
 
-    if args.test_flight is None:
-        plt.savefig(test_dir + "/sequence.png")
+    plt.savefig(test_dir + "/sequence.png")
 
     # Saving single images
-    if args.test_flight is None:
-        plt.figure().clear()
-        plt.close()
-        plt.cla()
-        plt.clf()
+    plt.figure().clear()
+    plt.close()
+    plt.cla()
+    plt.clf()
 
-        for i, frame in enumerate(x[0]):
-            plt.matshow(frame[0].cpu().detach().numpy())
-            plt.savefig(test_dir + "/{}.png".format(i))
+    for i, frame in enumerate(x[0]):
+        plt.matshow(frame[0].cpu().detach().numpy())
+        plt.savefig(test_dir + "/{}.png".format(i))
 
-        plt.matshow(outputs[0,0,0,:,:].cpu().detach().numpy())
-        plt.savefig(test_dir + "/predicted.png")
+    plt.matshow(outputs[0,0,0,:,:].cpu().detach().numpy())
+    plt.savefig(test_dir + "/predicted.png")
 
-        plt.matshow(y[0, 0, 0, :, :].cpu().detach().numpy())
-        plt.savefig(test_dir + "/ground_truth.png")
+    plt.matshow(y[0, 0, 0, :, :].cpu().detach().numpy())
+    plt.savefig(test_dir + "/ground_truth.png")
 
     # Write stats
     text_file = open("runs/" + foldername + "/test_{}/scores.txt".format(t), "w")
     n = text_file.write("SSIM: {}\nL1: {}\nMSE:{}".format(curr_ssim, curr_l1, curr_l2))
     text_file.close()
 
+    del x
+    del y
+    del img1
+    del img2
+    th.cuda.empty_cache()
     # ----------------
 
 ssim_score = ssim_score/args.n_tests
@@ -264,23 +241,3 @@ stats = "SSIM: {}\nL1: {}\nMSE:{}\nAvg.Inference Time: {}".format(ssim_score, l1
 text_file = open("runs/" + foldername + "/avg_score.txt", "w")
 n = text_file.write("SSIM: {}\nL1: {}\nMSE:{}".format(ssim_score, l1_score, l2_score))
 text_file.close()
-
-# ------------------------------
-'''
-print("[x] Starting inference...")
-true_means = []
-predicted_means = []
-for i, frame in enumerate(X[j][k]):
-    true_means.append(frame.cpu().detach().numpy().mean())
-    predicted_means.append(outputs[k, 0, i].cpu().detach().numpy().mean())
-
-plt.clf()
-plt.plot(range(len(true_means)), true_means,  "-b", label="True frames mean")
-plt.plot(range(len(true_means)), true_means,  "*")
-
-plt.plot(range(len(predicted_means)), predicted_means,  "-g", label="Predicted frames mean")
-plt.plot(range(len(predicted_means)), predicted_means,  "*")
-plt.grid()
-plt.legend()
-plt.savefig("runs/" + foldername + "/eval_means_{}.png".format(j))
-'''
